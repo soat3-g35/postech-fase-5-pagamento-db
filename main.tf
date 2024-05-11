0db8497fcd74e0f190ab409fd2bd01c6013deab6
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-05-10 21:59:32 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,30 +3,30 @@ provider "aws" {
   profile = "DynamoDB"
 }
 
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = ["g35-vpc"]
-  }
-}
+# data "aws_vpc" "selected" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["g35-vpc"]
+#   }
+# }
 
-data "aws_subnet_ids" "g35_subnets" {
-  vpc_id = data.aws_vpc.selected.id
-}
+# data "aws_subnet_ids" "g35_subnets" {
+#   vpc_id = data.aws_vpc.selected.id
+# }
 
-data "aws_subnet" "g35_subnet" {
-  for_each = data.aws_subnet_ids.g35_subnets.ids
-  id       = each.value
-}
+# data "aws_subnet" "g35_subnet" {
+#   for_each = data.aws_subnet_ids.g35_subnets.ids
+#   id       = each.value
+# }
 
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.g35_subnet : s.cidr_block]
-}
+# output "subnet_cidr_blocks" {
+#   value = [for s in data.aws_subnet.g35_subnet : s.cidr_block]
+# }
 
-resource "aws_vpc_endpoint" "pagamento" {
-  subnet_ids = data.aws_subnet_ids
-  vpc_id     = data.aws_vpc.selected.id
-}
+# resource "aws_vpc_endpoint" "dynamodb" {
+#   subnet_ids = data.aws_subnet_ids
+#   vpc_id     = data.aws_vpc.selected.id
+# }
 
 resource "aws_dynamodb_table" "pagamento" {
 
